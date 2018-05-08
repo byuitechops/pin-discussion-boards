b91cd8df6296c5bc210d1e472f97873aa27a7bec
Seth Childers <sethchilders92@gmail.com>
2018-05-08 13:06:51 -0600

    Added testing

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -3,13 +3,27 @@ const tap = require('tap');
 const canvas = require('canvas-wrapper');
 
 module.exports = (course, callback) => {
-    tap.test('child-template', (test) => {
+    tap.test('pin-discussion-boards', (test) => {
 
-        test.pass('potato');
-        test.pass('tomato');
-        test.fail('avacado');
+        /* Get all of the discussions */
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/discussion_topics`, (err, discussions) => {
+            if (err) {
+                getCallback(err);
+                return;
+            }
+            /* Two test cases. If the discussion being tested matches one of 
+            the test cases, and is in the correct position, then the test passes */
+            discussions.forEach(discussion => {
+                if (discussion.title === 'W03 Discussion: Another test for Pin Discussion Boards') {
+                    test.equal(1, discussion.position);
+                }
+                if (discussion.title === 'W06 Discussion: Pin Discussion Boards') {
+                    test.equal(2, discussion.position);
+                }
+            });
 
-        test.end();
+            test.end();
+        });
     });
 
     // Always call the callback in your childTests with just null
